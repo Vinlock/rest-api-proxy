78b22a0867e4aaaecbc050a1165e74041fdfd96b
Vinlock <dak@pvpallday.com>
2019-07-03 14:14:25 -0400

    Requests work

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // @flow
 import http from 'http';
 import https from 'https';
+import { URL } from 'url';
+import jitson from 'jitson';
 
 type RestProxyResponse = {
   body: ?string | ?{},
@@ -17,6 +19,7 @@ type RestProxyOptions = {
   host: string,
   port: ?number,
   headers: {},
+  parseJson: boolean,
 };
 
 type RestProxyRequest = (path: ?string, options: ?RestProxyRequestOptions) => Promise<RestProxyResponse>;
@@ -30,49 +33,70 @@ type RestProxyInstance = {
 };
 
 const restProxy = (options: ?RestProxyOptions = {}): RestProxyInstance => {
+  // Gather variables
+  let { host, headers, parseJson } = options;
   // Create instance of http or https
-  const isSecure = options.host.startsWith('https://');
+  const isSecure = host.startsWith('https://');
   let client = http;
   if (isSecure) client = https;
 
+  // Get Port
+  const port = options.port || (isSecure ? 443 : 80);
+
   // Return an axios style instance that accepts a
   // path, additional headers, body, method, and query params
-  const defaultOptions = {
-    port: isSecure ? 443 : 80,
-  };
-  return {
-    get: async (path: ?string, options: ?RestProxyRequestOptions = defaultOptions): Promise<RestProxyResponse> => {
+  return (method: string, path: ?string = '/', options: ?RestProxyRequestOptions = { parseJson: false }): Promise<RestProxyResponse> => {
+    // Gather variables
+    const { headers: additionalHeaders, query, data } = options;
 
+    const url = new URL(host);
 
-      return {
-        body: '',
-        headers: {},
-      };
-    },
-    post: async (path: ?string, options: ?RestProxyRequestOptions = defaultOptions): Promise<RestProxyResponse> => {
-      return {
-        body: '',
-        headers: {},
-      };
-    },
-    head: async (path: ?string, options: ?RestProxyRequestOptions = defaultOptions): Promise<RestProxyResponse> => {
-      return {
-        body: '',
-        headers: {},
-      };
-    },
-    delete: async (path: ?string, options: ?RestProxyRequestOptions = defaultOptions): Promise<RestProxyResponse> => {
-      return {
-        body: '',
-        headers: {},
-      };
-    },
-    put: async (path: ?string, options: ?RestProxyRequestOptions = defaultOptions): Promise<RestProxyResponse> => {
-      return {
-        body: '',
-        headers: {},
-      };
-    },
+    // Build data
+    let dataToSend = data || '';
+    if (data === Object(data)) {
+      try {
+        dataToSend = JSON.stringify(data);
+      } catch (err) {
+        dataToSend = data;
+      }
+    }
+
+    // Build options
+    const requestOptions = {
+      hostname: url.hostname,
+      port: url.port || port,
+      headers: {
+        ...headers,
+        ...additionalHeaders,
+        'Content-Length': dataToSend.length,
+      },
+      method, path,
+    };
+
+    // Request
+    return new Promise((resolve, reject) => {
+      const request =  client.request(requestOptions, (response) => {
+        response.on('data', (responseBody) => {
+          if (parseJson) {
+            const parse = jitson();
+            responseBody = parse(responseBody);
+          }
+
+          resolve({
+            body: responseBody,
+            headers: response.headers,
+            statusCode: response.statusCode,
+          });
+        });
+      });
+
+      request.on('error', (error) => {
+        reject(error);
+      });
+
+      request.write(data);
+      request.end();
+    });
   };
 };
 
--- src/index.test.js
+++ src/index.test.js
@@ -15,53 +15,6 @@ describe('restProxy', () => {
     })).toBeInstanceOf(Function);
   });
 
-  describe('RestProxyInstance', () => {
-    it('should have get as a function', () => {
-      return expect(restProxy({
-        host: 'https://api.test.com',
-        headers: {
-          'X-Auth-Token': '1234',
-        },
-      }).get).toBeInstanceOf(Function);
-    });
-
-    it('should have post as a function', () => {
-      return expect(restProxy({
-        host: 'https://api.test.com',
-        headers: {
-          'X-Auth-Token': '1234',
-        },
-      }).post).toBeInstanceOf(Function);
-    });
-
-    it('should have put as a function', () => {
-      return expect(restProxy({
-        host: 'https://api.test.com',
-        headers: {
-          'X-Auth-Token': '1234',
-        },
-      }).put).toBeInstanceOf(Function);
-    });
-
-    it('should have head as a function', () => {
-      return expect(restProxy({
-        host: 'https://api.test.com',
-        headers: {
-          'X-Auth-Token': '1234',
-        },
-      }).head).toBeInstanceOf(Function);
-    });
-
-    it('should have delete as a function', () => {
-      return expect(restProxy({
-        host: 'https://api.test.com',
-        headers: {
-          'X-Auth-Token': '1234',
-        },
-      }).delete).toBeInstanceOf(Function);
-    });
-  });
-
   describe('RestProxyResponse', () => {
     afterEach(() => {
       nock.cleanAll();
@@ -113,6 +66,31 @@ describe('restProxy', () => {
         },
       });
       return expect(proxy('GET', '/info', {})).resolves.toHaveProperty('headers');
+    });
+
+    it('should return correct response object from parsing the json', () => {
+      // Mock request
+      nock('https://api.test.com', {
+        'X-Auth-Token': '1234',
+      }).defaultReplyHeaders({
+        'Content-Type': 'application/json',
+      }).get('/info')
+        .reply(200, {
+          info: 'received',
+          status: true,
+        }, {
+          'Content-Type': 'application/json',
+        });
+
+      // Proxy
+      const proxy = restProxy({
+        host: 'https://api.test.com',
+        headers: {
+          'X-Auth-Token': '1234',
+        },
+        parseJson: true,
+      });
+      return expect(proxy('GET', '/info', {})).resolves.toHaveProperty('body.info');
     });
   });
 });
